be3c8e3bc7a7e01744ba9410f76806568953ba59
jiweihong0 <jiweihong0@gmail.com>
2024-03-11 17:49:58 +0800

    fix: router

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 // react router
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Page1 from "./page1";
 import Page2 from "./page2";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Page1 />} />
         <Route path="/page2" element={<Page2 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
